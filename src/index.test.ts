12b6101595b4918fac6aef0b1a3660f213f87d30
Justin Livi <silaslivi@gmail.com>
2019-02-06 22:16:18 -0500

    Add test for createKeyableReducer

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,15 +1,37 @@
-import { createActionCreator, FluxStandardAction } from '.';
+import { createActionCreator, createKeyableReducer, FluxStandardAction, ReducerMethod } from '.';
+
+const ACTION_TYPE = 'ACTION_TYPE';
+interface Action extends FluxStandardAction<typeof ACTION_TYPE> {
+  value: string;
+}
 
 describe('createActionCreator', () => {
-  const ACTION_TYPE = 'ACTION_TYPE';
-  interface Action extends FluxStandardAction<typeof ACTION_TYPE> {}
-
   it('should return the action creator', () => {
-    const actionCreator = (): { type: typeof ACTION_TYPE } => ({
-      type: ACTION_TYPE
+    const actionCreator = (): Action => ({
+      type: ACTION_TYPE,
+      value: 'value'
     });
     expect(createActionCreator<undefined, Action>(actionCreator)).toEqual(
       actionCreator
     );
+  });
+});
+
+describe('createKeyableReducer', () => {
+  it('should return a keyable reducer', () => {
+    interface State {
+      value: string;
+    }
+    const reducer: ReducerMethod<State, Action, 'ACTION_TYPE'> = (
+      state,
+      action
+    ) => ({
+      ...state,
+      ...action
+    });
+    expect(createKeyableReducer<State, Action>(ACTION_TYPE, reducer)).toEqual({
+      reducer,
+      type: ACTION_TYPE
+    });
   });
 });
